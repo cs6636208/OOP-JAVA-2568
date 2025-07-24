6f1d3e2259fa56dca8a675fdf5451577c4cbb17a
Your Name <you@example.com>
2025-07-24 18:57:11 +0700

    update Palindrome,java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,21 +2,20 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String text1 = input.nextLine();
-        input.close();
-        text1 = text1.replaceAll("[^a-zA-Z0-9]", "").replace(" ", "");
-        System.out.println(isPalindrome(text1) ? 1 : 0);
+        Scanner scanner = new Scanner(System.in);
+        String text1 = scanner.nextLine();
+        scanner.close();
+        String cleanedText = text1.replaceAll("[^a-zA-Z]", "").toUpperCase();
+        System.out.println((isPalindrome(cleanedText) ? 0 : 1));
     }
 
     public static boolean isPalindrome(String s) {
         int length = s.length();
-        for (int i = 0; i < (int) (length / 2); i++) {
+        for (int i = 0; i < length / 2; i++) {
             if (s.charAt(i) != s.charAt(length - 1 - i)) {
                 return false;
             }
         }
         return true;
     }
-
 }
